83d25745c9085a718e59727b9c2d8080b15703c4
Ronny Polloqueri Anco <ronny.polloqueri@gmail.com>
2021-06-13 15:09:25 -0500

    Temporal solution

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -37995,7 +37995,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35215" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36825" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

8d5928517e04aad28f5d5b52e2919a48ae312fd3
Ronny Polloqueri Anco <ronny.polloqueri@gmail.com>
2021-06-13 12:47:30 -0500

    Generate questions from a dictionary

--- src/components/Quiz/utils.js
+++ src/components/Quiz/utils.js
@@ -1,27 +1,51 @@
-export const questions = [
-  {
-    question: "Cual es el lago mas alto de Peru?",
-    alternatives: ["Huacachina", "Titicaca", "Aricota"],
-    answer: "Titicaca"
-  },
-  {
-    question: "Cual es el rio mas caudaloso de Peru?",
-    alternatives: ["Amazonas", "Uchusuma", "Marañon", "Ucayali"],
-    answer: "Amazonas"
-  },
-  {
-    question: "Cual de los siguientes platos no es peruano?",
-    alternatives: [
-      "Ceviche",
-      "Cuy Chactado",
-      "Porotos a la rienda",
-      "Lomo saltado"
-    ],
-    answer: "Porotos a la rienda"
-  },
-  {
-    question: "En que año se declaro la independencia de Peru?",
-    alternatives: ["1821", "1822", "1823", "1824"],
-    answer: "1821"
+export const animals = {
+  "cangrejo" : "crab",
+  "ballena": "whale",
+  "coral": "coral",
+  "delfin": "dolphin",
+  "foca": "seal",
+  "tortuga": "turtle",
+  "mariscos": "shellfish"
+}
+
+export const generateQuestions = (dictionary, numberQuestions) => {
+  const keys = Object.keys(dictionary);
+  const alternativesQuantity = 4;
+  const mainShuffleKeys = shuffle(keys);
+  let shuffleKeys = [...mainShuffleKeys];
+  let questions = [];
+  let key;
+  let value;
+  let newShuffleKeys;
+  while(shuffleKeys.length > 0 && questions.length < numberQuestions){
+    key = shuffleKeys.pop();
+    value = dictionary[key];
+    newShuffleKeys = shuffle([... mainShuffleKeys]);
+    let alternatives = newShuffleKeys.slice(0, alternativesQuantity - 1).map(key => dictionary[key])
+    const randomPosition = Math.floor(Math.random() * alternativesQuantity);
+    insertAt(alternatives, randomPosition, value);
+
+    questions.push({
+      'question': `Como se dice ${key} en ingles?`,
+      'alternatives': alternatives,
+      'answer': value
+    })
   }
-];
+  console.log('questions', questions);
+  return questions;
+}
+
+const insertAt = (array, index, ...elementsArray) => {
+  array.splice(index, 0, ...elementsArray);
+}
+
+/*
+ * https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ * Warning!
+ * The use of this algorithm is not recommended, because it is inefficient 
+ * and strongly biased; see comments. It is being left here for future 
+ * reference, because the idea is not that rare.
+*/
+const shuffle = (array)=> {
+  return array.sort( () => .5 - Math.random() );
+}
